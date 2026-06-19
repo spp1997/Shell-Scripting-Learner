8cb0449abb7a2cee786f8c5714cec53110c8dc8b
ubuntu <suprhashi188797s@gmail.com>
2026-06-19 14:12:39 +0530

    without assiging variable to read, the default inbuilt variable REPLY takes user input

---
+++ reply.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/bash
+
+# when we not have variable to read the user input 
+# read     
+# the input value go to inbuilt variable REPLY (Default Variable)
+
+echo "Enter name: "
+read 
+echo "Name: $REPLY"
+
+
+
+
+
+
