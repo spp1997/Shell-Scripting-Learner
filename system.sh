35ec533e2f4304a869202357e0e9fe364e2260f9
ubuntu <suprhashi188797s@gmail.com>
2026-06-19 11:26:33 +0530

    added display sentenace to system variables

--- system.sh
+++ system.sh
@@ -5,4 +5,10 @@ echo $BASH_VERSION
 echo $HOME
 echo $PWD
 
+echo Our shell name is $BASH
+echo Our shell version name is $BASH_VERSION
+echo Our home directory is $HOME
+echo Our Current working directory is $PWD
+
+
 
