18b4265df342169ed1d35f2b304db8a5f1cefb6a
spp1997 <suprhashi188797s@gmail.com>
2026-07-01 04:56:42 +0000

    Numbers example

---
+++ numbers.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/bash
+
+#Number Example
+num1=5
+num2=10
+sum=$((num1+num2))
+difference=$((num2-num1))
+product=$((num1*num2))
+quotient=$((num2/num1))
+echo "Sum: $sum, difference: $difference, Product: $product, Quotient: $quotient"
+
+
